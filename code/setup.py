86f995bcb7e3685a78395b41a278583a36f97e9a
Don Sheehy <don.r.sheehy@gmail.com>
2019-12-18 15:21:36 -0500

    Moved the code files into the correct folder

---
+++ code/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="greedypermutation",
+    version="0.0.1",
+    author="Donald R. Sheehy",
+    author_email="don.r.sheehy@gmail.com",
+    description="A package for computing greedy permutations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://donsheehy.net",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
