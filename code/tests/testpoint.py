98b1124605b815bb159de0d57569dfa6711b02d1
Don Sheehy <don.r.sheehy@gmail.com>
2019-12-18 11:24:09 -0500

    Test Infrastructure.

---
+++ code/tests/testpoint.py
@@ -0,0 +1,9 @@
+from context import greedypermutation
+import unittest
+
+class TestPoint(unittest.TestCase):
+    def testiniteuclideanpoint(self):
+        assert(False)
+
+if __name__ == '__main__':
+    unittest.main()
