d97f97b16abfec20a7a378f5f93381439a025ae4
Don Sheehy <don.r.sheehy@gmail.com>
2019-12-19 16:12:24 -0500

    Added proper tests to the Point class and fixed the bugs.

--- code/greedypermutation/point.py
+++ code/greedypermutation/point.py
@@ -1,12 +1,27 @@
 class Point:
-    def __init__(self, *coords):
-        self._p = list(coords)
+    """A simple class to describe points in Euclidean space with float
+    coordinates.
+    """
+
+    def __init__(self, coords):
+        self._p = tuple(float(x) for x in coords)
+
+    @staticmethod
+    def fromstring(s):
+        return Point(x for x in s.split())
 
     def dist(self, other):
-        return (sum(a*a + b*b for (a,b) in zip(self, other))) ** (0.5)
+        """Compute the Euclidean distance between the `self` and `other`.
+        If the dimensions don't match the distance is computed in projection
+        down to the common subspace.
+        """
+        return (sum((a-b)**2 for (a,b) in zip(self, other))) ** (0.5)
+
+    def __eq__(self, other):
+        return self._p == other._p
 
     def __hash__(self):
-        return sum(hash(c) for c in self)
+        return hash(self._p)
 
     def __iter__(self):
         return iter(self._p)
@@ -14,8 +29,11 @@ class Point:
     def __str__(self):
         return "(" + ", ".join(str(c) for c in self._p) +  ")"
 
+    def __len__(self):
+        return len(self._p)
+
 if __name__ == '__main__':
-    p = Point(0,0)
-    q = Point(3,4)
+    p = Point([0,0])
+    q = Point.fromstring('3.3 4.4')
     print(p.dist(q))
-    print(p)
+    print(q)
--- code/tests/testpoint.py
+++ code/tests/testpoint.py
@@ -3,10 +3,66 @@ import unittest
 from greedypermutation import Point
 
 class TestPoint(unittest.TestCase):
-    def testiniteuclideanpoint(self):
-        assert(False)
+    def test_init_2D_points(self):
+        # Just checking that init doesn't crash
+        Point([0,0])
+        Point([-1,2])
+        Point([-1.1, 2.99])
 
-    
+    def testlen(self):
+        P = [Point([1]), Point([1,2]), Point([1,2,3])]
+        self.assertEqual(len(P[0]), 1)
+        self.assertEqual(len(P[1]), 2)
+        self.assertEqual(len(P[2]), 3)
+
+    def test_init_with_other_iterables(self):
+        p = Point(range(100))
+        self.assertEqual(len(p), 100)
+
+    def test_dist_1D(self):
+        P = [Point([i]) for i in range(10)]
+        self.assertEqual(P[1].dist(P[5]), 4)
+        self.assertEqual(P[5].dist(P[1]), 4)
+        self.assertEqual(P[0].dist(P[9]), 9)
+        self.assertEqual(P[1].dist(P[1]), 0)
+        self.assertEqual(P[6].dist(P[8]), 2)
+
+    def test_dist_2D(self):
+        a = Point([1,3])
+        b = Point([13,8])
+        self.assertEqual(a.dist(b), 13)
+
+    def test_dist_with_different_dimensions(self):
+        """If two points are in different dimensions, the higher dimensional
+        point should be projected to the lower dimensional space.
+        """
+        p, q = Point([3,4]), Point([0])
+        self.assertEqual(p.dist(q), 3)
+        r = Point([0,0,100])
+        self.assertEqual(p.dist(r), 5)
+
+    def test_eq(self):
+        a = Point([3,4])
+        b = Point([3.0, 4.0])
+        self.assertTrue(a == b)
+
+    def test_hash(self):
+        a = Point([30000000000,4])
+        b = Point([30000000000.0, 4.0])
+        self.assertEqual(hash(a), hash(b))
+
+    def test_fromstring(self):
+        p = Point.fromstring("  4  2.01 999")
+        self.assertEqual(len(p), 3)
+        a = Point.fromstring(' 3  4  12')
+        self.assertEqual(len(a), 3)
+        b = Point([0,0,0])
+        self.assertEqual(a.dist(b), 13)
+
+    def test_iter(self):
+        p = Point(3* i for i in range(4))
+        for i, c in enumerate(p):
+            self.assertEqual(c, 3 * i)
 
 if __name__ == '__main__':
     unittest.main()
