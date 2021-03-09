2c0a5b3b2524646af15baa8193164812dc1b5ecc
donsheehy <don.r.sheehy@gmail.com>
2021-03-09 07:52:19 -0500

    Bumped version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="greedypermutation",
-    version="0.3.0",
+    version="0.4.0",
     author="Donald R. Sheehy",
     author_email="don.r.sheehy@gmail.com",
     description="A package for computing greedy permutations",
