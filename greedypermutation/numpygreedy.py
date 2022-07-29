3423c19253f34aefb74c15e5752698059f6d0b13
donsheehy <don.r.sheehy@gmail.com>
2022-07-29 16:01:17 -0400

    added sampler to numpygreedy.

--- greedypermutation/numpygreedy.py
+++ greedypermutation/numpygreedy.py
@@ -1,11 +1,23 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 
-def greedy(P):
-    S = cdist(P, P, 'sqeuclidean')
+def greedy(P, distance = 'sqeuclidean'):
+    S = cdist(P, P, distance)
     D = S[0]
     j = 0
     for _ in P:
         yield P[j]
         j = D.argmax()
         D = np.minimum(S[j], D)
+
+def sample(P, delta, distance = 'sqeuclidean'):
+    S = cdist(P, P, distance)
+    D = S[0]
+    j = 0
+    output = []
+    for _ in P:
+        output.append(P[j])
+        j = D.argmax()
+        D = np.minimum(S[j], D)
+        if D[j] < delta:
+            return output
