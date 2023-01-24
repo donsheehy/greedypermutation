bf591c540af6f7e0d8972014376d2b142334c686
Oliver Chubet <oliver.chubet@gmail.com>
2023-01-24 14:55:44 -0500

    added parameters to greedy tree decorator

--- greedypermutation/decorators.py
+++ greedypermutation/decorators.py
@@ -1,9 +1,9 @@
 def greedytree_class(cls):
-  def buildtree(M, seed=None, name=None):
+  def buildtree(M, seed=None, nbrconstant=1, moveconstant=1, name=None):
     BallTree = Ball(M)
     gp = greedy(M)
     class Inner(cls):
-      root = greedy_tree(M, seed)
+      root = greedy_tree(M, seed=seed, nbrconstant=nbrconstant, moveconstant=moveconstant)
     if name is None:
       name = cls.__name__ + '_' + M.__class__.__name__
       Inner.__name__ = Inner.__qualname__ = name
--- greedypermutation/greedytreeclass.py
+++ greedypermutation/greedytreeclass.py
@@ -1,14 +1,20 @@
+"""
+Intended usage example:
+>>> g = GreedyTreeClass(X, seed=Point((1,2)), nbrconstant=2, moveconstant=2)
+>>> g.refine(param)
+"""
+
 @greedytree_class
 class GreedyTreeClass:
   @classmethod
-  def refine(cls):
+  def refine(cls, param):
     pass
 
   @classmethod
-  def merge(cls):
+  def merge(cls, other):
     pass
 
   @classmethod
-  def delete(cls):
+  def delete(cls, point):
     pass
 
